ca44a817c68af5745eb1fad563bf35e955e7db27
Gcat101 <Gcatmail2@gmail.com>
2022-02-19 00:13:04 +0300

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo "Installing..."
 pip install -r ./requirements.txt # Install pip modules
+mkdir /opt/tui-shop # Create opt directory
 cp ./tui-shop.py /usr/bin/tui-shop # Copy bin
 chmod +x /usr/bin/tui-shop # Make install file executable
 
